764af6fafb12cb6cafb6dfe30aac5f08682e494a
Timchenko <tbd2504@gmail.com>
2022-06-14 22:50:08 +0300

    first commit

--- task_1.py
+++ task_1.py
@@ -1,10 +1,22 @@
-INCREASE = 1.1
-count_day = 0
-distance = int(input('Введіть число скільки пробіг в перший день спортсмен (км): '))
-search_distance = int(input('Введіть число скільки повинен пробігти спортсмен (км): '))
+def get_input_number(message: str) -> int:
+    ret = ''
+    while not ret.isdigit():
+        ret = input(message)
+    return int(ret)
 
-while distance < search_distance:
-    distance *= INCREASE
-    count_day += 1
 
-print(f'Спортсмен досяг відмітки {search_distance}(км) у {count_day + 1} день.')
+def main():
+    INCREASE = 1.1
+    count_day = 0
+    distance = get_input_number('Введіть число скільки пробіг в перший день спортсмен (км): ')
+    search_distance = get_input_number('Введіть число скільки повинен пробігти спортсмен (км): ')
+
+    while distance < search_distance:
+        distance *= INCREASE
+        count_day += 1
+
+    print(f'Спортсмен досяг відмітки {search_distance}(км) у {count_day + 1} день.')
+
+
+if __name__ == '__main__':
+    main()
