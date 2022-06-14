78fe640890553b8d4293aebd4f358e8a18fe7470
Timchenko <tbd2504@gmail.com>
2022-06-15 01:55:32 +0300

    first commit

---
+++ task_6.py
@@ -0,0 +1,13 @@
+from random import randint
+
+some_list = [randint(0, 10) for i in range(randint(10, 50))]
+
+count = 0
+
+for i, _ in enumerate(some_list):
+    if 0 < i < len(some_list) - 1:
+        if some_list[i - 1] < some_list[i] > some_list[i + 1]:
+            count += 1
+
+print(some_list)
+print(count)
