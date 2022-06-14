62510bffd6fd0f209e4e01803bec67b00e028298
Timchenko <tbd2504@gmail.com>
2022-06-14 22:40:01 +0300

    first commit

--- task_1.py
+++ task_1.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+INCREASE = 1.1
+count_day = 0
+distance = int(input('Введіть число скільки пробіг в перший день спортсмен (км): '))
+search_distance = int(input('Введіть число скільки повинен пробігти спортсмен (км): '))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+while distance < search_distance:
+    distance *= INCREASE
+    count_day += 1
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f'Спортсмен досяг відмітки {search_distance}(км) у {count_day + 1} день.')
