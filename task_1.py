561c1422e7154febf2871b650fd73caacaa0ee40
Timchenko <tbd2504@gmail.com>
2022-06-15 01:06:20 +0300

    second commit

--- task_1.py
+++ task_1.py
@@ -5,15 +5,19 @@ def get_input_number(message: str) -> int:
     return int(ret)
 
 
+def get_count_day(some_distance: int, some_search_distance: int, some_increase: float) -> int:
+    count = 0
+    while some_distance < some_search_distance:
+        some_distance *= some_increase
+        count += 1
+    return count
+
+
 def main():
     INCREASE = 1.1
-    count_day = 0
     distance = get_input_number('Введіть число скільки пробіг в перший день спортсмен (км): ')
     search_distance = get_input_number('Введіть число скільки повинен пробігти спортсмен (км): ')
-
-    while distance < search_distance:
-        distance *= INCREASE
-        count_day += 1
+    count_day = get_count_day(distance, search_distance, INCREASE)
 
     print(f'Спортсмен досяг відмітки {search_distance}(км) у {count_day + 1} день.')
 
