64159942cae1ae135d2b815323163b15f701c8eb
Timchenko <tbd2504@gmail.com>
2022-06-15 02:44:39 +0300

    second commit

--- task_4.py
+++ task_4.py
@@ -1,4 +1,13 @@
-some_str = ''
-for n in range(1, 10):
-    some_str += str(n)
-    print(some_str)
+def print_stairs(start: int, end: int):
+    some_str = ''
+    for i in range(start, end):
+        some_str += str(i)
+        print(some_str)
+
+
+def main():
+    print_stairs(1, 9)
+
+
+if __name__ == '__main__':
+    main()
