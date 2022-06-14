01a40baefaf1801b4b21dddc968ac48ed9d75cd3
Timchenko <tbd2504@gmail.com>
2022-06-15 01:41:07 +0300

    first commit

---
+++ task_5.py
@@ -0,0 +1,18 @@
+def get_alphabet():
+    ret = ''
+    for elem in range(ord('A'), ord('Z') + 1):
+        ret += chr(elem)
+
+    return ret
+
+alphabet = get_alphabet()
+print(alphabet)
+print(f'Третій символ: {alphabet[2]}')
+print(f'Предостаній символ: {alphabet[-2]}')
+print(f"Перші п'ять символів: {alphabet[0:5]}")
+print(f'Весь рядок крім двох останіх символів: {alphabet[0:-2]}')
+print(f'Символи з парними індексами: {alphabet[0::2]}')
+print(f'Символи з не парними індексами: {alphabet[1::2]}')
+print(f'Перевернутий рядок: {alphabet[::-1]}')
+print(f'Перевернутий рядок з кожним другим симоволом: {alphabet[-1::-2]}')
+print(f'Довжина рядка: {len(alphabet)}')
